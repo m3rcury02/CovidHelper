d6476824c41f68c6d36d9a146f67487438a64346
m3rcury02 <gunal2002@gmail.com>
2021-04-28 09:34:37 +0530

    changes

--- app/src/main/java/com/example/covidhelper/MainActivity.kt
+++ app/src/main/java/com/example/covidhelper/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.covidhelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val Reqbutton : Button = findViewById(R.id.button2)
     }
 }
