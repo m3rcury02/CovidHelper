2185f134bac5f7c224e9c3550f0b29d5b420bf39
vamseevk <vamseevk2001@gmail.cmo>
2021-04-28 09:39:03 +0530

    added toast

--- app/src/main/java/com/example/covidhelper/MainActivity.kt
+++ app/src/main/java/com/example/covidhelper/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.covidhelper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val Reqbutton : Button = findViewById(R.id.button2)
+        Reqbutton.setOnClickListener{
+          Toast.makeText(this, "this is a request button !!", Toast.LENGTH_LONG).show()
+        }
     }
 }
