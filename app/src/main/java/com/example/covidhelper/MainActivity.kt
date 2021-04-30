30283e5578ef78127b7aac45fe018583905c7dc0
vamseevk <vamseevk2001@gmail.cmo>
2021-05-01 00:45:42 +0530

    Floating Action Button

--- app/src/main/java/com/example/covidhelper/MainActivity.kt
+++ app/src/main/java/com/example/covidhelper/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.covidhelper
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
@@ -10,9 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val reqButton : Button = findViewById(R.id.button2)
-        reqButton.setOnClickListener{
-          Toast.makeText(this, "this is a request button !!", Toast.LENGTH_LONG).show()
-        }
     }
+
+    fun request(view: View) {}
+    fun donate(view: View) {}
 }
