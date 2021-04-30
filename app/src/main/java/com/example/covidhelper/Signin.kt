41cb7e7350da5e520072bd6242e1025dd869cf2a
vamseevk <vamseevk2001@gmail.cmo>
2021-05-01 00:25:15 +0530

    skip signin

--- app/src/main/java/com/example/covidhelper/Signin.kt
+++ app/src/main/java/com/example/covidhelper/Signin.kt
@@ -1,11 +1,19 @@
 package com.example.covidhelper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signin)
+    }
+
+    fun skipSignin(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
