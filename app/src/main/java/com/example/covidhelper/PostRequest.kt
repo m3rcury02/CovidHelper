5d0d97c9e339873981263290c6cb7da47fdd9110
SHARVESH.S <sharveshsreethar0806@gmail.com>
2021-05-01 09:46:55 +0530

    added details about donate

--- app/src/main/java/com/example/covidhelper/PostRequest.kt
+++ app/src/main/java/com/example/covidhelper/PostRequest.kt
@@ -2,10 +2,29 @@ package com.example.covidhelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class PostRequest : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_request)
-    }
-}
+        val languages = resources.getStringArray(R.array.Languages)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, languages)
+            spinner.adapter = adapter
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View, position: Int, id: Long) {
+                }
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+}}}
