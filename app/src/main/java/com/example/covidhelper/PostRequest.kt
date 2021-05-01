0a76589e2d8f7be5ce7ad3a58f37a73204d5c21a
vamseevk <vamseevk2001@gmail.cmo>
2021-05-01 19:41:36 +0530

    now user can post Request and see all the requests in the main screen

--- app/src/main/java/com/example/covidhelper/PostRequest.kt
+++ app/src/main/java/com/example/covidhelper/PostRequest.kt
@@ -14,22 +14,8 @@ class PostRequest : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_post_request)
-        val languages = resources.getStringArray(R.array.Languages)
-        val spinner = findViewById<Spinner>(R.id.spinner)
-        if (spinner != null) {
-            val adapter = ArrayAdapter(this,
-                android.R.layout.simple_spinner_item, languages)
-            spinner.adapter = adapter
-            spinner.onItemSelectedListener = object :
-                AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>,
-                                            view: View, position: Int, id: Long) {
-                }
-                override fun onNothingSelected(parent: AdapterView<*>) {
-                    // write code to perform some action
-                }
-            }
-}}
+    }
+
 
     fun postRequest(view: View) {
         val name = name.text.toString().trim()
